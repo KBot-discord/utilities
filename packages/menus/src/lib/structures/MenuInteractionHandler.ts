e74875c4d0fca15e159cd645ab2648ec77f1168a
killbasa <killbasab@gmail.com>
2022-12-21 17:23:57 -0500

    Add defer options

--- packages/menus/src/lib/structures/MenuInteractionHandler.ts
+++ packages/menus/src/lib/structures/MenuInteractionHandler.ts
@@ -4,21 +4,37 @@ import type { MessageComponentInteraction } from 'discord.js';
 
 export abstract class MenuInteractionHandler extends InteractionHandler {
 	private readonly customIdPrefix: string;
+	private readonly defer: DeferOptions | undefined;
+	private readonly ephemeral: boolean | undefined;
 
 	protected constructor(context: InteractionHandler.Context, options: MenuInteractionHandlerOptions) {
 		super(context, options);
 		this.customIdPrefix = options.customIdPrefix;
+		this.defer = options.defer;
+		this.ephemeral = options.ephemeral;
 	}
 
-	public override parse(interaction: MessageComponentInteraction) {
+	public override async parse(interaction: MessageComponentInteraction) {
 		if (!interaction.customId.startsWith(this.customIdPrefix)) return this.none();
 		const data = parseCustomId(interaction.customId);
+		if (this.defer === DeferOptions.Reply) {
+			await interaction.deferReply({ ephemeral: this.ephemeral });
+		} else if (this.defer === DeferOptions.Update) {
+			await interaction.deferUpdate();
+		}
 		return this.some(data);
 	}
 }
 
+export const enum DeferOptions {
+	Reply = 'reply',
+	Update = 'update'
+}
+
 export interface MenuInteractionHandlerOptions extends InteractionHandler.Options {
 	customIdPrefix: string;
+	defer?: DeferOptions;
+	ephemeral?: boolean;
 }
 
 export namespace MenuInteractionHandler {
