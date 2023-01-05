09402a28f7be3631718c524eccb62a45342ce025
killbasa <killbasab@gmail.com>
2023-01-05 16:28:08 -0500

    Update vitest configs

--- packages/custom-id/vitest.config.ts
+++ packages/custom-id/vitest.config.ts
@@ -3,12 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		globals: true,
-		include: ['./tests/**/*.test.ts'],
-		root: './',
-		reporters: ['default', 'junit'],
-		outputFile: './coverage/junit.xml'
-	},
-	esbuild: {
-		target: 'es2021'
+		coverage: {
+			reporter: ['text', 'lcov', 'clover']
+		}
 	}
 });
