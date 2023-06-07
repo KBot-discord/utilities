25fe23a3aacdb9f402b37f2956c9c85448f67ae3
Shane <killbasab@gmail.com>
2023-06-07 16:02:52 -0400

    feat: create config packages (#23)

--- packages/menus/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/structures/Menu';
-export * from './lib/structures/MenuPageBuilder';
-export * from './lib/structures/MenuPagesBuilder';
-export * from './lib/types/MenuPageTypes';
--- packages/menus/src/lib/util/isNullOrUndefined.ts
+++
@@ -1,3 +0,0 @@
-export function isNullOrUndefined(value: unknown): value is null | undefined {
-	return value === undefined || value === null;
-}
